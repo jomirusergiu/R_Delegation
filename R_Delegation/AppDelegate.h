2fc1f3d061891bfdf4d99b3c531883827beb84bf
Jomiru Sergiu <jomirusergiu@gmail.com>
2013-12-03 23:23:31 +0200

    Initial Commit

---
+++ R_Delegation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  R_Delegation
+//
+//  Created by Admin on 12/3/13.
+//  Copyright (c) 2013 RocKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ R_Delegation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  R_Delegation
+//
+//  Created by Admin on 12/3/13.
+//  Copyright (c) 2013 RocKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
